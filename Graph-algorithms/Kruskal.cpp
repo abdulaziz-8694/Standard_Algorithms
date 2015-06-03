7cad51faccc7b3a6d3a0715a36d4e67e3e20b4f4
abdulaziz <cs12b1001@iith.ac.in>
2015-06-03 12:02:25 +0530

    Update Kruskal.cpp

--- Graph-algorithms/Kruskal.cpp
+++ Graph-algorithms/Kruskal.cpp
@@ -30,8 +30,8 @@ bool sort_by_weight(const Edge &first,const Edge &second)
     return (first.weight<second.weight);
 }
 class DisjointSets{// implementing Disjoint Sets Data Structure with path compression and rank
-private:
-    int* Parent;//representative of elements
+private
+	int* Parent;//representative of elements
 	int* Rank;//rank of elements
 //Constructor for DisjointSets whilch will create numElements distinct sets
 public:
@@ -46,28 +46,30 @@ public:
 			Rank[i]=0;
 		}
 	}
+	
 	int Find(int i) {//Finding the representing of the set containing i
-
-    // If i is the parent of itself
-    if (Parent[i] == i) {
-
-        // Then i is the representative of his set
-        return i;
-    }
-    else { // Else if i is not the parent of itself
-
-        // Then i is not the representative of his set,
-        // so we recursively call Find on it's parent, and save it in our result variable
-        int result = Find(Parent[i]);
-
-        // We then cache the result by moving i's node directly under the representative of his set
-        Parent[i] = result;
-
-        // And then we return the result
-        return result;
-    }
-}
-//to take union of two sets
+	
+		// If i is the parent of itself
+		if (Parent[i] == i) {
+	
+	        // Then i is the representative of his set
+	        	return i;
+	    	}
+		else { // Else if i is not the parent of itself
+	
+		        // Then i is not the representative of his set,
+		        // so we recursively call Find on it's parent, and save it in our result variable
+		        int result = Find(Parent[i]);
+		
+		        // We then cache the result by moving i's node directly under the representative of his set
+		        Parent[i] = result;
+		
+		        // And then we return the result
+		        return result;
+		 }
+	}
+	
+	//to take union of two sets
 	void Union(int u,int v)
 	{
 		int repu=Find(u);
